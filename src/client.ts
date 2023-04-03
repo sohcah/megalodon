419bb146dd56e94baca66a0e7393a54731e0c3e1
Sam Hindess <mail@sohcah.dev>
2023-04-03 22:16:29 +0100

    Revert Temporary Patch, Upgrade Discord.JS

--- src/client.ts
+++ src/client.ts
@@ -8,187 +8,187 @@ import {writeFileSync} from "fs";
 import textToSpeech from "@google-cloud/text-to-speech";
 
 if (process.env.TTS_SERVICE_ACCOUNT) {
-    writeFileSync(path.resolve("/megalodon-sa.json"), process.env.TTS_SERVICE_ACCOUNT);
+	writeFileSync(path.resolve("/megalodon-sa.json"), process.env.TTS_SERVICE_ACCOUNT);
 }
 const ttsClient = new textToSpeech.TextToSpeechClient({
-    keyFile: process.env.MEGDEV
-        ? path.resolve(__dirname, "../serviceaccount.json")
-        : path.resolve("/megalodon-sa.json"),
+	keyFile: process.env.MEGDEV
+		? path.resolve(__dirname, "../serviceaccount.json")
+		: path.resolve("/megalodon-sa.json"),
 });
 import {TTSClientController} from "./controller";
 import {TTSClientSettings, TTSGuildOptions, User} from "./types";
 import {
-    AudioPlayer,
-    AudioPlayerStatus,
-    createAudioPlayer,
-    createAudioResource,
-    getVoiceConnection,
-    joinVoiceChannel,// NoSubscriberBehavior,
+	AudioPlayer,
+	AudioPlayerStatus,
+	createAudioPlayer,
+	createAudioResource,
+	getVoiceConnection,
+	joinVoiceChannel,// NoSubscriberBehavior,
 } from "@discordjs/voice";
 import {ApplicationCommandDataResolvable} from "discord.js";
 import {p} from "./prisma";
 
 export class TTSClient {
-    guilds: Map<string, TTSGuildOptions> = new Map();
-    client: Discord.Client;
-    controller: TTSClientController;
-    primary: boolean;
-    settings: TTSClientSettings;
+	guilds: Map<string, TTSGuildOptions> = new Map();
+	client: Discord.Client;
+	controller: TTSClientController;
+	primary: boolean;
+	settings: TTSClientSettings;
 
-    private log(...args: unknown[]) {
-        console.log(`[🤖 ${this.client.user?.username}]`, ...args);
-    }
+	private log(...args: unknown[]) {
+		console.log(`[🤖 ${this.client.user?.username}]`, ...args);
+	}
 
-    constructor(
-        controller: TTSClientController,
-        settings: TTSClientSettings,
-        primary?: boolean
-    ) {
-        this.controller = controller;
-        this.primary = primary || false;
-        this.settings = settings;
+	constructor(
+		controller: TTSClientController,
+		settings: TTSClientSettings,
+		primary?: boolean
+	) {
+		this.controller = controller;
+		this.primary = primary || false;
+		this.settings = settings;
 
-        this.client = new Discord.Client({
-            allowedMentions: {
-                parse: [],
-                users: [],
-                roles: [],
-                repliedUser: false,
-            },
-            intents: [
-                Discord.IntentsBitField.Flags.GuildMessages,
-                Discord.IntentsBitField.Flags.GuildVoiceStates,
-                Discord.IntentsBitField.Flags.Guilds,
-                Discord.IntentsBitField.Flags.GuildEmojisAndStickers,
-                Discord.IntentsBitField.Flags.MessageContent,
-            ],
-        });
+		this.client = new Discord.Client({
+			allowedMentions: {
+				parse: [],
+				users: [],
+				roles: [],
+				repliedUser: false,
+			},
+			intents: [
+				Discord.IntentsBitField.Flags.GuildMessages,
+				Discord.IntentsBitField.Flags.GuildVoiceStates,
+				Discord.IntentsBitField.Flags.Guilds,
+				Discord.IntentsBitField.Flags.GuildEmojisAndStickers,
+				Discord.IntentsBitField.Flags.MessageContent,
+			],
+		});
 
-        this.client.login(settings.token);
+		this.client.login(settings.token);
 
-        this.client.on("ready", async () => {
-            this.log("Ready!");
-            const channel = this.client.channels.cache.get("792460755504070666");
-            const embed = new Discord.EmbedBuilder();
-            embed.setAuthor({
-                name: this.client.user?.username ?? "N/A",
-                iconURL: this.client.user?.avatarURL() || undefined
-            });
-            embed.setTitle(`${this.controller.name} Activated`);
-            embed.setColor("#00ff00");
-            embed.setTimestamp(Date.now());
-            if (channel instanceof Discord.TextChannel) channel.send({embeds: [embed]});
-            if (this.primary) {
-                const commands: ApplicationCommandDataResolvable[] = [
-                    {
-                        name: this.controller.command,
-                        description: `Toggle ${this.controller.name}`,
-                        type: Discord.ApplicationCommandType.ChatInput,
-                    },
-                    {
-                        name: `Read Message`,
-                        type: Discord.ApplicationCommandType.Message,
-                    },
-                    {
-                        name: `${this.controller.command}config`,
-                        description: `Change your ${this.controller.name} settings`,
-                        type: Discord.ApplicationCommandType.ChatInput,
-                        options: [
-                            {
-                                name: "name",
-                                description: `Set your name for ${this.controller.name}`,
-                                type: 1,
-                                required: false,
-                                options: [
-                                    {
-                                        name: "name",
-                                        description: "Your Name",
-                                        type: 3,
-                                        required: true,
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                ];
+		this.client.on("ready", async () => {
+			this.log("Ready!");
+			const channel = this.client.channels.cache.get("792460755504070666");
+			const embed = new Discord.EmbedBuilder();
+			embed.setAuthor({
+				name: this.client.user?.username ?? "N/A",
+				iconURL: this.client.user?.avatarURL() || undefined
+			});
+			embed.setTitle(`${this.controller.name} Activated`);
+			embed.setColor("#00ff00");
+			embed.setTimestamp(Date.now());
+			if (channel instanceof Discord.TextChannel) channel.send({embeds: [embed]});
+			if (this.primary) {
+				const commands: ApplicationCommandDataResolvable[] = [
+					{
+						name: this.controller.command,
+						description: `Toggle ${this.controller.name}`,
+						type: Discord.ApplicationCommandType.ChatInput,
+					},
+					{
+						name: `Read Message`,
+						type: Discord.ApplicationCommandType.Message,
+					},
+					{
+						name: `${this.controller.command}config`,
+						description: `Change your ${this.controller.name} settings`,
+						type: Discord.ApplicationCommandType.ChatInput,
+						options: [
+							{
+								name: "name",
+								description: `Set your name for ${this.controller.name}`,
+								type: 1,
+								required: false,
+								options: [
+									{
+										name: "name",
+										description: "Your Name",
+										type: 3,
+										required: true,
+									},
+								],
+							},
+						],
+					},
+				];
 
-                this.client.guilds.resolve(this.controller.devguild as `${bigint}`)?.commands.set(commands);
-                this.client.application?.commands.set(commands);
+				this.client.guilds.resolve(this.controller.devguild as `${bigint}`)?.commands.set(commands);
+				this.client.application?.commands.set(commands);
 
-                this.client.on("interactionCreate", async interaction => {
-                    if (
-                        interaction.isCommand() &&
-                        (interaction.commandName === this.controller.command ||
-                            interaction.commandName === this.controller.command + "dev")
-                    ) {
-                        const autoID = `${interaction.channelId}_${interaction.member?.user.id}`;
-                        if (this.controller.auto.has(autoID)) {
-                            this.controller.auto.delete(autoID);
-                            interaction.reply({
-                                content: `${this.controller.name} Disabled in <#${interaction.channelId}> for ${interaction.member?.user.username}`,
-                                allowedMentions: {parse: []},
-                            });
-                        } else {
-                            this.controller.auto.add(autoID);
-                            interaction.reply({
-                                content: `${this.controller.name} Enabled in <#${interaction.channelId}> for ${interaction.member?.user.username}`,
-                                allowedMentions: {parse: []},
-                            });
-                            return;
-                        }
-                    } else if (
-                        interaction.isCommand() &&
-                        (interaction.commandName === this.controller.command + "config" ||
-                            interaction.commandName === this.controller.command + "configdev")
-                    ) {
-                        const nameOption = interaction.options.get("name");
-                        if (nameOption) {
-                            const name = nameOption.value;
-                            const user = await p.users.findUnique({where: {id: interaction.member?.user.id}});
-                            if (user) {
-                                await p.users.update({
-                                    where: {id: interaction.member?.user.id},
-                                    data: {name: name as string}
-                                })
-                            } else {
-                                await p.users.create({
-                                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                                    data: {id: interaction.member!.user.id, name: name as string}
-                                })
-                            }
-                            interaction.reply({
-                                content: `Set ${interaction.member?.user.username}'s Name to ${name}`,
-                                allowedMentions: {parse: []},
-                            });
-                            return;
-                        }
-                    } else if (
-                        interaction.isContextMenuCommand() &&
-                        (interaction.commandName === "Read Message")
-                    ) {
-                        controller.handleRead(interaction);
-                    }
-                });
-            }
-        });
+				this.client.on("interactionCreate", async interaction => {
+					if (
+						interaction.isCommand() &&
+						(interaction.commandName === this.controller.command ||
+							interaction.commandName === this.controller.command + "dev")
+					) {
+						const autoID = `${interaction.channelId}_${interaction.member?.user.id}`;
+						if (this.controller.auto.has(autoID)) {
+							this.controller.auto.delete(autoID);
+							interaction.reply({
+								content: `${this.controller.name} Disabled in <#${interaction.channelId}> for ${interaction.member?.user.username}`,
+								allowedMentions: {parse: []},
+							});
+						} else {
+							this.controller.auto.add(autoID);
+							interaction.reply({
+								content: `${this.controller.name} Enabled in <#${interaction.channelId}> for ${interaction.member?.user.username}`,
+								allowedMentions: {parse: []},
+							});
+							return;
+						}
+					} else if (
+						interaction.isCommand() &&
+						(interaction.commandName === this.controller.command + "config" ||
+							interaction.commandName === this.controller.command + "configdev")
+					) {
+						const nameOption = interaction.options.get("name");
+						if (nameOption) {
+							const name = nameOption.value;
+							const user = await p.users.findUnique({where: {id: interaction.member?.user.id}});
+							if (user) {
+								await p.users.update({
+									where: {id: interaction.member?.user.id},
+									data: {name: name as string}
+								})
+							} else {
+								await p.users.create({
+									// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+									data: {id: interaction.member!.user.id, name: name as string}
+								})
+							}
+							interaction.reply({
+								content: `Set ${interaction.member?.user.username}'s Name to ${name}`,
+								allowedMentions: {parse: []},
+							});
+							return;
+						}
+					} else if (
+						interaction.isContextMenuCommand() &&
+						(interaction.commandName === "Read Message")
+					) {
+						controller.handleRead(interaction);
+					}
+				});
+			}
+		});
 
-        this.client.on("messageCreate", message => controller.handleMessage(message, this));
-    }
+		this.client.on("messageCreate", message => controller.handleMessage(message, this));
+	}
 
-    async disconnect(): Promise<void> {
-        const channel = this.client.channels.cache.get("792460755504070666");
-        const embed = new Discord.EmbedBuilder();
-        embed.setAuthor({
-            name: this.client.user?.username as `${bigint}`,
-            iconURL: this.client.user?.avatarURL() || undefined
-        });
-        embed.setTitle(`${this.controller.name} Deactivated`);
-        embed.setColor("#ff0000");
-        embed.setTimestamp(Date.now());
-        if (channel instanceof Discord.TextChannel) await channel.send({embeds: [embed]});
-        this.client.destroy();
-        return;
-    }
+	async disconnect(): Promise<void> {
+		const channel = this.client.channels.cache.get("792460755504070666");
+		const embed = new Discord.EmbedBuilder();
+		embed.setAuthor({
+			name: this.client.user?.username as `${bigint}`,
+			iconURL: this.client.user?.avatarURL() || undefined
+		});
+		embed.setTitle(`${this.controller.name} Deactivated`);
+		embed.setColor("#ff0000");
+		embed.setTimestamp(Date.now());
+		if (channel instanceof Discord.TextChannel) await channel.send({embeds: [embed]});
+		this.client.destroy();
+		return;
+	}
 
     async playSSML(
         ssml: string,
@@ -218,31 +218,14 @@ export class TTSClient {
         });
 
         this.log("Getting Voice Connection");
-        const connection = getVoiceConnection(channel.guild.id, channel.guild.members.me?.id) ?? await (async () => {
-            const conn = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator as any,
-                group: channel.guild.members.me?.id,
-                selfDeaf: true,
-                selfMute: false,
-            });
-            // Temporary resolution for DiscordJS/Voice bug caused by Discord's UDP Keep Alive packets
-            // https://github.com/discordjs/discord.js/issues/9185#issuecomment-1452514375
-            conn.on('stateChange', (oldState, newState) => {
-                const oldNetworking = Reflect.get(oldState, 'networking');
-                const newNetworking = Reflect.get(newState, 'networking');
-
-                const networkStateChangeHandler = (oldNetworkState: any, newNetworkState: any) => {
-                    const newUdp = Reflect.get(newNetworkState, 'udp');
-                    clearInterval(newUdp?.keepAliveInterval);
-                }
-
-                oldNetworking?.off('stateChange', networkStateChangeHandler);
-                newNetworking?.on('stateChange', networkStateChangeHandler);
-            });
-            return conn;
-        })();
+		const connection = getVoiceConnection(channel.guild.id, channel.guild.members.me?.id) ?? await joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guild.id,
+			adapterCreator: channel.guild.voiceAdapterCreator as any,
+			group: channel.guild.members.me?.id,
+			selfDeaf: true,
+			selfMute: false,
+		});
         if (connection.joinConfig.channelId !== channel.id) {
             connection.rejoin({
                 channelId: channel.id,
@@ -270,52 +253,52 @@ export class TTSClient {
             }
             this.log("Audio Played");
 
-            let guildSettings = this.guilds.get(channel.guild.id);
-            if (!guildSettings) {
-                guildSettings = {};
-            }
+			let guildSettings = this.guilds.get(channel.guild.id);
+			if (!guildSettings) {
+				guildSettings = {};
+			}
 
-            guildSettings.last_user = user.id;
-            guildSettings.last_time = Date.now();
-            guildSettings.speaking = Date.now();
+			guildSettings.last_user = user.id;
+			guildSettings.last_time = Date.now();
+			guildSettings.speaking = Date.now();
 
-            // Auto-Disconnect after 5 Minutes
-            if (channel.guild.id !== this.controller.devguild)
-                channel.guild.members.me?.setNickname(
-                    `${channel.guild.members.me.displayName?.split(" | ")[0]} | ${
-                        settings?.name || channel.guild.members.resolve(user.id)?.displayName || user.username
-                    }`.slice(0, 32)
-                );
+			// Auto-Disconnect after 5 Minutes
+			if (channel.guild.id !== this.controller.devguild)
+				channel.guild.members.me?.setNickname(
+					`${channel.guild.members.me.displayName?.split(" | ")[0]} | ${
+						settings?.name || channel.guild.members.resolve(user.id)?.displayName || user.username
+					}`.slice(0, 32)
+				);
 
-            this.log("Setting Timeout");
+			this.log("Setting Timeout");
 
-            if (guildSettings.timeout) clearTimeout(guildSettings.timeout);
-            guildSettings.timeout = setTimeout(() => {
-                connection.disconnect();
-                if (channel.guild.id !== this.controller.devguild)
-                    channel.guild.members.me?.setNickname(channel.guild.members.me.displayName?.split(" | ")[0].slice(0, 32));
-            }, 300000);
+			if (guildSettings.timeout) clearTimeout(guildSettings.timeout);
+			guildSettings.timeout = setTimeout(() => {
+				connection.disconnect();
+				if (channel.guild.id !== this.controller.devguild)
+					channel.guild.members.me?.setNickname(channel.guild.members.me.displayName?.split(" | ")[0].slice(0, 32));
+			}, 300000);
 
-            const thisTimeout = guildSettings.timeout;
-            audioPlayer.on(AudioPlayerStatus.Idle, () => {
-                if (
-                    channel.guild.id !== this.controller.devguild &&
-                    thisTimeout === guildSettings?.timeout
-                ) {
-                    channel.guild.members.me?.setNickname(channel.guild.members.me.displayName?.split(" | ")[0].slice(0, 32));
-                    guildSettings.speaking = undefined;
-                }
-            });
+			const thisTimeout = guildSettings.timeout;
+			audioPlayer.on(AudioPlayerStatus.Idle, () => {
+				if (
+					channel.guild.id !== this.controller.devguild &&
+					thisTimeout === guildSettings?.timeout
+				) {
+					channel.guild.members.me?.setNickname(channel.guild.members.me.displayName?.split(" | ")[0].slice(0, 32));
+					guildSettings.speaking = undefined;
+				}
+			});
 
-            audioPlayer.on("error", err => {
-                this.log("Audio Player Error", err);
-                audioPlayer.stop();
-            });
+			audioPlayer.on("error", err => {
+				this.log("Audio Player Error", err);
+				audioPlayer.stop();
+			});
 
-            this.guilds.set(channel.guild.id, guildSettings);
+			this.guilds.set(channel.guild.id, guildSettings);
 
-            this.log("Done");
-        }
-        return;
-    }
+			this.log("Done");
+		}
+		return;
+	}
 }
